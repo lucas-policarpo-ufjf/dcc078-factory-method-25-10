892dfc1dcf27fab030f3c9584918e1e3a460a189
lucas-policarpo-ufjf <114711262+lucas-policarpo-ufjf@users.noreply.github.com>
2022-10-25 22:24:01 -0300

    Delete LayoutCommerce.java

--- src/main/java/LayoutCommerce.java
+++
@@ -1,9 +0,0 @@
-public class LayoutCommerce {
-    public String render(){
-        return "layout commerce";
-    }
-
-    public String updateRender(String str){
-        return "layout commerce " + str;
-    }
-}
